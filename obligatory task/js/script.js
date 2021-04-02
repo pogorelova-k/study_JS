0db90f99a118a4c5dcefe4e695e34c89b5b6dc8a
Ksenia Pogorelova <ksenia.harina5@gmail.com>
2021-04-02 13:51:50 +0300

    Использование методов и свойств

--- obligatory task/js/script.js
+++ obligatory task/js/script.js
@@ -4,3 +4,17 @@ let money = 1000000,
     deposit = false, 
     mission = 13000000, 
     period = 12;
+
+console.log('money:', typeof money);
+console.log('income:', typeof income);
+console.log('deposit:', typeof deposit);
+
+console.log('длина строки addExpenses:', addExpenses.length);
+
+console.log('Период равен ', period, 'месяцев.', 'Цель заработать', mission, 'рублей' );
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = money / 30;
+
+console.log('budgetDay:', budgetDay);
