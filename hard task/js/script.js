6b783d7e3348e7d298d2ced5ef5f1d3e58c23115
Ksenia Pogorelova <ksenia.harina5@gmail.com>
2021-04-06 19:33:05 +0300

    Усложнённое задание урока 4

--- hard task/js/script.js
+++ hard task/js/script.js
@@ -1,55 +1,20 @@
 'use strict';
-let lang = 'en';
-let daysOfWeek = {};
-let namePerson = 'Ксюша';
 
-//todo  ---Вывод дней недели в зависимости от языка---- 
-//* через IF
-if (lang === 'ru') {
-    console.log( 
-        'if:',
-        'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'
-    );
-} else if (lang === 'en') {
-    console.log(
-        'if: ',
-        'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday'
-    );
-};
+let string = '  Lorem   ipsum, dolor sit  amet  adipisicing elit.  Lorem   ipsum, dolor sit  amet  adipisicing elit. ';
 
-//* через switch-case
-switch (lang) {
-    case 'ru':
-        console.log( 
-            'switch-case: ',
-            'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'
-        );
-        break;
-    case 'en':
-        console.log(
-            'switch-case: ',
-            'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday'
-        );
-        break;
-    default: 
-        console.log( 
-            'switch-case default: ',
-            'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'
-        );
-        break;
-};
+// Удаляем пробелы в начале и конце строки
+function deleteSpace(arg) {
+    if (typeof(arg) === 'string') {
+        if (arg.length > 30) {
+            arg = arg.trim().substr(0, 30).padEnd(33, '...');
+        } else {
+            arg = arg.trim();
+        }
+        
+    } else {
+        arg = 'Вы ввели строку не правильно';
+    }
+    return arg;
+}
 
-//* через мноомерный массив
-daysOfWeek = {
-    'ru': ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-    'en': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-};
-console.log('многомерный массив', daysOfWeek[lang]);
-
-
-//todo Тернарные операторы
-namePerson === 'Артём' ? console.log(namePerson,': директор') : namePerson === 'Максим' ? console.log(namePerson,': преподаватель') : console.log(namePerson,': студент');
-
-
-
-
+console.log(deleteSpace(string));
