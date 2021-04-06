e168b87f5b5fd11cd19d2e24f55531764d27e988
Ksenia Pogorelova <ksenia.harina5@gmail.com>
2021-04-06 13:26:31 +0300

    Первая часть усложнённого задания урока 3

--- hard task/js/script.js
+++ hard task/js/script.js
@@ -1,16 +1,47 @@
 'use strict';
-let num = 266219;
-let result = 1;
+let lang = 'en';
+let daysOfWeek = {};
 
-// Произведение цифр числа num
-while (num > 0) {
-    result = result * (num % 10);
-    num =  Math.trunc(num/10);
-    continue;
-}
+//todo  ---Вывод дней недели в зависимости от языка---- 
+//* через IF
+if (lang === 'ru') {
+    console.log( 
+        'if:',
+        'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'
+    );
+} else if (lang === 'en') {
+    console.log(
+        'if: ',
+        'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday'
+    );
+};
 
-console.log(result);
-// возведение в степень 
-console.log(result **= 3);
-// Вывод первых двух цифр числа
-console.log(String(result).slice(0, 2));
+//* через switch-case
+switch (lang) {
+    case 'ru':
+        console.log( 
+            'switch-case: ',
+            'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'
+        );
+        break;
+    case 'en':
+        console.log(
+            'switch-case: ',
+            'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday'
+        );
+        break;
+    default: 
+        console.log( 
+            'switch-case default: ',
+            'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'
+        );
+        break;
+};
+
+//* через мноомерный массив
+daysOfWeek = {
+    'ru': ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    'en': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+};
+
+console.log('многомерный массив', daysOfWeek[lang]);
