92cb922c0abcbdbcc47107409f1a86ef77f1380b
Ksenia Pogorelova <ksenia.harina5@gmail.com>
2021-04-06 14:37:53 +0300

    Исправлено условие на отрицательное значение budgetDay

--- obligatory task/js/script.js
+++ obligatory task/js/script.js
@@ -44,4 +44,6 @@ if (budgetDay >= 1200) {
     console.log('У вас средний уровень дохода');
 } else if (budgetDay < 600 && budgetDay >= 0 ) {
     console.log('К сожалению, у вас уровень дохода ниже среднего');
+} else {
+    console.log('Что то пошло не так');
 }
