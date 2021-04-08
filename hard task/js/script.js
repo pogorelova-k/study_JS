16de1bc052f5a6cdd4c90403ba49ee1cdb523536
Ksenia Pogorelova <ksenia.harina5@gmail.com>
2021-04-08 10:48:41 +0300

    Вывод простых чисел

--- hard task/js/script.js
+++ hard task/js/script.js
@@ -8,3 +8,14 @@ for (let i = 0; i < array.length; i++) {
     };
     
 };
+
+// 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+nextStep:
+for (let i = 2; i < 100; i++) {
+    for (let j = 2; j < i; j++) {
+        if ( i % j === 0 ) {
+            continue nextStep;
+        } 
+    }
+    console.log(i, ': Делители 1 и ', i);
+};
