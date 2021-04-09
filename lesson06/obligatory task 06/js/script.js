327824706250c01b94afbd86d8df51bdadc77f00
Ksenia Pogorelova <ksenia.harina5@gmail.com>
2021-04-09 13:14:00 +0300

    Добавлено ограничение попыток

--- lesson06/obligatory task 06/js/script.js
+++ lesson06/obligatory task 06/js/script.js
@@ -11,25 +11,44 @@ let isNumber = (n) => {
 
 function playWithBot() { 
     let botNumber = getRandomInt(100); //это должно быть замыканием 
+    let countAttempts = 1; // Счётчик попыток
+    let attempts = 10; // Количетсво попыток
     function checkNumber(userNumber) {
-        while (!isNumber(userNumber)) {
-            userNumber = +prompt('Введите число', 75);
-        }
-        if (userNumber === 0) {
+        if (countAttempts < attempts ) {
+            while (!isNumber(userNumber)) {
+                userNumber = +prompt('Введите число', 75);
+            }
+            if (userNumber === 0) {
+                alert('Игра окончена');
+                return;
+            }
+            if (userNumber > botNumber) {
+                alert('Загаданное число меньше, осталось ' + (attempts - countAttempts) + ' попыток(-ки)');
+            } else if (userNumber < botNumber) {
+                alert('Загаданное число больше, осталось ' + (attempts - countAttempts) + ' попыток(-ки)');
+            } else if (userNumber === botNumber) {
+                
+                if (confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')) {
+                    playWithBot();
+                } else {
+                    return;
+                }
+            }
+            countAttempts++;
+        } else if (userNumber === botNumber) {
+            if (confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')) {
+                playWithBot();
+            } else {
+            countAttempts = 1;
             alert('Игра окончена');
             return;
+            }
         }
-        if (userNumber > botNumber) {
-            alert('Загаданное число меньше');
-        } else if (userNumber < botNumber) {
-            alert('Загаданное число больше');
-        } else if (userNumber === botNumber) {
-            alert('Поздравляю, Вы угадали!!! ');
-            return;
-        }
-        console.log('userNumber:',userNumber, 'botNumber:', botNumber);
+        console.log('userNumber:',userNumber, 'botNumber:', botNumber, 'countAttempts:', countAttempts);
+
         checkNumber(+prompt('Угадай число от 1 до 100', 75));
     }
+    console.dir(checkNumber);
     return checkNumber(+prompt('Угадай число от 1 до 100', 75));
 }
 playWithBot();
