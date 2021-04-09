e0eea40866414246c64ce88e3fa5687f8f81b881
Ksenia Pogorelova <ksenia.harina5@gmail.com>
2021-04-09 12:28:55 +0300

    Игра с ботом с замыканием загаданного числа

--- lesson06/obligatory task 06/js/script.js
+++ lesson06/obligatory task 06/js/script.js
@@ -1,7 +1,5 @@
 'use strict';
 
-let  botNumber = getRandomInt(100);
-
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 };
@@ -11,26 +9,27 @@ let isNumber = (n) => {
     return !isNaN(parseFloat(n)) && isFinite(n); 
 };
 
-function playBot() {
-    let userNumber = +prompt('Угадай число от 1 до 100', 75);
-    while (!isNumber(userNumber)) {
-        userNumber = +prompt('Введите число', 75);
+function playWithBot() { 
+    let botNumber = getRandomInt(100); //это должно быть замыканием 
+    function checkNumber(userNumber) {
+        while (!isNumber(userNumber)) {
+            userNumber = +prompt('Введите число', 75);
+        }
+        if (userNumber === 0) {
+            alert('Игра окончена');
+            return;
+        }
+        if (userNumber > botNumber) {
+            alert('Загаданное число меньше');
+        } else if (userNumber < botNumber) {
+            alert('Загаданное число больше');
+        } else if (userNumber === botNumber) {
+            alert('Поздравляю, Вы угадали!!! ');
+            return;
+        }
+        console.log('userNumber:',userNumber, 'botNumber:', botNumber);
+        checkNumber(+prompt('Угадай число от 1 до 100', 75));
     }
-    if (userNumber === 0) {
-        alert('Игра окончена');
-        return;
-    }
-    if (userNumber > botNumber) {
-        alert('Загаданное число меньше');
-    } else if (userNumber < botNumber) {
-        alert('Загаданное число больше');
-    } else if (userNumber === botNumber) {
-        alert('Поздравляю, Вы угадали!!! ');
-        return;
-    }
-    console.log('userNumber:',userNumber, 'botNumber:', botNumber);
-    
-    playBot();
+    return checkNumber(+prompt('Угадай число от 1 до 100', 75));
 }
-
-playBot();
+playWithBot();
