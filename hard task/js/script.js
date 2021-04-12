66c13d5edd0e617b8bda69ab4a544d2e1ed5f6c8
Ksenia Pogorelova <ksenia.harina5@gmail.com>
2021-04-12 11:09:36 +0300

    Вывод дней недели на новой строке. Выходные курсивом

--- hard task/js/script.js
+++ hard task/js/script.js
@@ -1,21 +1,15 @@
 'use strict';
 
-let array = ['1234556', '2354343242', '23423425', '876353', '43288482', '98773', '348763'];
+let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскрсенье'];
+let dayOfWeek = document.querySelector('.day-of-week');
+let str = '';
 
-for (let i = 0; i < array.length; i++) {
-    if (array[i].charAt(0) === '2' || array[i].charAt(0) === '4') {
-        console.log(array[i]);
-    };
-    
-};
+week.forEach((item, i) => {
 
-// 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
-nextStep:
-for (let i = 2; i < 100; i++) {
-    for (let j = 2; j < i; j++) {
-        if ( i % j === 0 ) {
-            continue nextStep;
-        } 
-    }
-    console.log(i, ': Делители 1 и ', i);
-};
+    if (i === 5 || i === 6) {
+        item = '<i>'+ item +'</i>';
+    } 
+    str += item+'<br>';
+});
+
+dayOfWeek.innerHTML = str;
