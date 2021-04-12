829246b270b4a02bb8c31f0765fc68f898dc956d
Ksenia Pogorelova <ksenia.harina5@gmail.com>
2021-04-12 11:10:47 +0300

    текущий день выделен жирным шрифтом

--- hard task/js/script.js
+++ hard task/js/script.js
@@ -3,9 +3,12 @@
 let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскрсенье'];
 let dayOfWeek = document.querySelector('.day-of-week');
 let str = '';
+let today = new Date();
 
 week.forEach((item, i) => {
-
+    if (i === today.getDay() - 1) {
+        item = '<b>'+ item +'</b>';
+    }
     if (i === 5 || i === 6) {
         item = '<i>'+ item +'</i>';
     } 
