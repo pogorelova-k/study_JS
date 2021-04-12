a1d9c85cc6ce86ac5c8a4df01044c4bd83503a2d
Ksenia Pogorelova <ksenia.harina5@gmail.com>
2021-04-12 18:39:53 +0300

    правильные склонения в первой строчке

--- hard task/js/script.js
+++ hard task/js/script.js
@@ -1,56 +1,57 @@
 'use strict';
 
-let today = new Date;
-let date1 = document.querySelector('.date-1');
-let date2 = document.querySelector('.date-2');
-let week = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
-let months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", 
-            "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
-let hour = ['часа', 'часов', 'час'],
-    minute = ['минута', 'минуты', 'минут'],
-    second = ['секунда', 'секунды', 'секундо'];
+let today = new Date,
+    date1 = document.querySelector('.date-1'),
+    date2 = document.querySelector('.date-2'),
+    week = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"],
+    months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", 
+            "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
+    hours = ['час', 'часа', 'часов'],
+    minutes = ['минута', 'минуты', 'минут'],
+    seconds = ['секунда', 'секунды', 'секунд'],
+    day = today.getDate(),
+    month = today.getMonth(),
+    year = today.getFullYear(),
+    hour = today.getHours(),
+    minute = today.getMinutes(),
+    second = today.getSeconds();
+
 
  //  'Сегодня Вторник, 4 февраля 2020 года, 21 час 5 минут 33 секунды'  
-
+let fullDay =   'Сегодня ' + week[today.getDay()] + ', ' + 
+                day + ' ' + 
+                months[month] + ' ' + 
+                year  + ', ' + 
+                hour + ' ' + hours[declension(hours, hour)] + ' ' +
+                minute + ' ' + minutes[declension(minutes, minute)] + ' ' +
+                second + ' ' + seconds[declension(seconds, second)];
 
                 // '04.02.2020 - 21:05:33' 
-let partDay =   today.getDate() + '.' + 
-                today.getMonth() + '.' + 
-                today.getFullYear()  + ' - ' + 
-                today.getHours() + ':' + 
-                today.getMinutes() + ':' + 
-                today.getSeconds();
+let partDay =   day + '.' + 
+                month + '.' + 
+                year  + ' - ' + 
+                hour + ':' + 
+                minute + ':' + 
+                second;
 
-var decCache = [],
-    decCases = [2, 0, 1, 1, 1, 2];
-function decOfNum(number, titles)
-{
-    if(!decCache[number]) decCache[number] = number % 100 > 4 && number % 100 < 20 ? 2 : decCases[Math.min(number % 10, 5)];
-    return titles[decCache[number]];
+// Функция для определения индекса массива hours
+// val - это значение минуты, часа или секунды
+// forms - массив, откуда выбирать по индексу нужное слово с правильным окончанием
+function declension(forms, val) {
+    const cases = [ 2, 0, 1, 1, 1, 2];
+    if ((val % 100 > 4 && val % 100 < 20)) {
+        return 2;
+    } else if (val % 10 < 5) {
+        return cases[val % 10];
+    } else {
+        return cases[5];
+    }
+    return forms[(val % 100 > 4 && val % 100 < 20) ? 2 : cases[(val % 10 < 5) ? val % 10 : 5]];
 }
 
-const changeDeclination = (number) => {
-    // if(!decCache[number]) {
-    //     if (decCache[number] == number % 100 > 4 && number % 100 < 20) {
-    //         decCache[number] = 2;
-    //     } else {
-    //         decCases[Math.min(number % 10, 5)];
-    //     }
-    // }    
-
-    let fullDay =   'Сегодня ' + week[today.getDay()] + ', ' + 
-                    today.getDate() + ' ' + 
-                    months[today.getMonth()] + ' ' + 
-                    today.getFullYear()  + ', ' + 
-                    today.getHours() + ' ' + hour[number] + ' ' +
-                    today.getMinutes() + ' ' + minute[number] + ' ' +
-                    today.getSeconds() + ' ' + second[number];
-
-    date1.innerHTML = fullDay;
-};
+console.log('declension(hours, hour);: ', declension(hours, hour));
 
 
-changeDeclination();
 
-
+date1.innerHTML = fullDay;
 date2.innerHTML = partDay;
