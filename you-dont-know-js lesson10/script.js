4971f00dce2f774814eb3a92225653f9974c676c
Ksenia Pogorelova <ksenia.harina5@gmail.com>
2021-04-13 13:07:09 +0300

    Удалена реклама со страницы

--- you-dont-know-js lesson10/script.js
+++ you-dont-know-js lesson10/script.js
@@ -9,4 +9,7 @@ books.append(book[2]);
 book[3].before(book[4]);
 
 body.style.backgroundImage = 'url(./image/adv.jpg)';
+
 book[4].querySelector('a').textContent = 'Книга 3. this и Прототипы Объектов';
+
+body.querySelector('.adv').remove();
