5be36d41ec3a6dac8bf715c0fc68d9c33fe71240
Ksenia Pogorelova <ksenia.harina5@gmail.com>
2021-04-13 13:05:37 +0300

    Исправлен заголовок 3 книги

--- you-dont-know-js lesson10/script.js
+++ you-dont-know-js lesson10/script.js
@@ -3,12 +3,10 @@
 const books = document.querySelector('.books');
 const book = document.querySelectorAll('.book');
 const body = document.querySelector('body');
-console.log('body: ', body);
 
 books.prepend(book[1]);
 books.append(book[2]);
 book[3].before(book[4]);
 
 body.style.backgroundImage = 'url(./image/adv.jpg)';
-
-
+book[4].querySelector('a').textContent = 'Книга 3. this и Прототипы Объектов';
