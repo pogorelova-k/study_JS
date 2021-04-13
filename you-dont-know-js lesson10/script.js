6c06ac20ea4641cd4fc37a510ff47f8d56de8491
Ksenia Pogorelova <ksenia.harina5@gmail.com>
2021-04-13 12:55:31 +0300

    Изменен фон

--- you-dont-know-js lesson10/script.js
+++ you-dont-know-js lesson10/script.js
@@ -2,10 +2,13 @@
 
 const books = document.querySelector('.books');
 const book = document.querySelectorAll('.book');
-console.log('book: ', book);
+const body = document.querySelector('body');
+console.log('body: ', body);
 
 books.prepend(book[1]);
 books.append(book[2]);
 book[3].before(book[4]);
+
+body.style.backgroundImage = 'url(./image/adv.jpg)';
 
 
