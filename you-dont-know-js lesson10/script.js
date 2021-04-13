d14756c7285ddc8ac65fe7f44b2e1dbb64c9e930
Ksenia Pogorelova <ksenia.harina5@gmail.com>
2021-04-13 13:46:55 +0300

    В 6 книге добавлена глава и упорядочена в списке

--- you-dont-know-js lesson10/script.js
+++ you-dont-know-js lesson10/script.js
@@ -5,7 +5,7 @@ const book = document.querySelectorAll('.book');
 const body = document.querySelector('body');
 const chaptersBookTwo = book[0].querySelectorAll('li');
 const chaptersBookFive = book[5].querySelectorAll('li');
-console.log('chaptersBookFive: ', chaptersBookFive);
+
 
 
 books.prepend(book[1]);
@@ -25,7 +25,7 @@ chaptersBookFive[1].after(chaptersBookFive[9]);
 chaptersBookFive[4].after(chaptersBookFive[2]);
 chaptersBookFive[7].after(chaptersBookFive[5]);
 
-
-
-
-
+const newChapterBookEight = chaptersBookTwo[6].cloneNode();
+newChapterBookEight.textContent = 'Глава 8: За пределами ES6';
+book[2].querySelector('ul').append(newChapterBookEight);
+book[2].querySelectorAll('li')[9].before(book[2].querySelectorAll('li')[10]);
