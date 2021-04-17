9c7a341bb36349b8fd92301b3c3f215f48494b4f
Ksenia Pogorelova <ksenia.harina5@gmail.com>
2021-04-17 14:32:35 +0300

    Двигающийся квадрат

--- obligatory task/part 1/index.js
+++ obligatory task/part 1/index.js
@@ -15,16 +15,47 @@ DomElement.prototype.createElement = function () {
         elem.classList.add(this.selector);
     } else if (this.selector.slice(0, 1) === '#') {
         elem = document.createElement('div');
-        elem.id = this.selector;
+        elem.id = this.selector.slice(1);
+    } else {
+        elem = document.createElement('div');
     }
     elem.textContent = this.selector;
     elem.style.cssText = `  height: ${this.height}; 
                             width: ${this.width};
                             background: ${this.bg};
-                            font-size: ${this.fontSize};`;
+                            font-size: ${this.fontSize};
+                            `;
     document.body.append(elem);
 };
 
-const domElem = new DomElement('#bold', '300px', '500px', 'mediumturquoise', '140px');
+const domElem = new DomElement('#text', '100px', '100px', 'mediumturquoise', '140px');
 
-domElem.createElement();
+document.addEventListener("DOMContentLoaded", () => {
+    domElem.createElement();
+
+    domElem.position = 'absolute';
+    domElem.bg = 'red';
+    
+    const div = document.querySelector('div');
+    div.style.cssText = `position: ${domElem.position};
+                        background: ${domElem.bg};
+                        height: ${domElem.height};
+                        width: ${domElem.width};`;
+    
+    document.addEventListener('keydown', event => {
+        const   e = window.event,
+                key = e.key;
+        if (key === 'ArrowUp') {
+            div.style.top = '10px';
+        } else if (key === 'ArrowDown'){
+            div.style.top = '20px';
+        } else if (key === 'ArrowLeft'){
+            div.style.left = '10px';
+        } else if (key === 'ArrowRight'){
+            div.style.left = '20px';
+        }
+    });
+
+});
+
+
