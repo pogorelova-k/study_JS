18cd73652c2093c6f6c4e1638e551bac0bc268e0
Ksenia Pogorelova <ksenia.harina5@gmail.com>
2021-04-30 12:27:47 +0300

    Исправлен баг при валидации форм

--- js/scipt.js
+++ js/scipt.js
@@ -9,53 +9,77 @@ window.addEventListener('DOMContentLoaded', () => {
 		userInputs = document.querySelectorAll('input');
 
 	userInputs.forEach(input => {
-		input.addEventListener('blur', () => {
-			// убираем удваивающиеся пробелы и дефисы
-			input.value = input.value.replace((/[-]+/gi), '-');
-			input.value = input.value.replace((/[\s]+/gi), ' ');
+		input.addEventListener('input', () => {
 
-			// удаляем пробелы и дефисы в начале и конце
+			// убираем удваивающиеся пробелы и дефисы во время написания
+			input.value = input.value.replace((/[-]+/gi), '-');
+			input.value = input.value.replace((/[\s]+/g), ' ');
+
+			if (input.getAttribute('name') === 'user_name') {
+				// разрешен только ввод кириллицы в любом регистре, дефиса и пробела
+				input.value = input.value.replace((/[^а-яА-Я-\s]/), '');
+				// Первая буква заглавная, остальные прописные
+				input.value = input.value.replace(/(|\s+)\S/g, val => val.toLowerCase());
+				input.value = input.value.replace(/(^|\s)\S/g, val => val.toUpperCase());
+
+			} else if (input.getAttribute('name') === 'user_message') {
+				// разрешен только ввод кириллицы в любом регистре, дефиса и пробела
+				input.value = input.value.replace((/[^а-яА-Я-\s]/), '');
+
+			} else if (input.getAttribute('name') === 'user_email') {
+				// разрешен только ввод ввод латиницы в любом регистре и спецсимволы
+				input.value = input.value.replace((/[^a-zA-z@-_.!~*']/gi), '');
+
+			} else if (input.getAttribute('name') === 'user_phone') {
+				// разрешен только ввод цифр, круглых скобок и дефис
+				input.value = input.value.replace((/[^\d()-]+/), '');
+			}
+		});
+
+		input.addEventListener('blur', () => {
+
+			// удаляем пробелы и дефисы в начале и конце вне фокуса инпута без вставки
 			input.value = input.value.replace((/^-/), '');
 			input.value = input.value.replace((/-$/), '');
+			input.value = input.value.replace((/^\s/gi), '');
 			input.value = input.value.replace((/\s$/gi), '');
 
 			if (input.getAttribute('name') === 'user_name') {
-				if ((/[а-яА-Я\s-]/).test(input.value)) {
+				if ((/[^а-яА-Я\s-]/).test(input.value)) {
 					input.value = input.value.replace((/[^а-яА-Я-\s]/gi), '');
 					input.value = input.value.replace(/(|\s+)\S/g, val => val.toLowerCase());
 					input.value = input.value.replace(/(^|\s)\S/g, val => val.toUpperCase());
+					// убираем удваивающиеся пробелы и дефисы вне фокуса после вставки
+					input.value = input.value.replace((/[\s]+/g), ' ');
+					input.value = input.value.replace((/[-]+/gi), '-');
 				}
-				input.addEventListener('input', () => {
-					// разрешен только ввод кириллицы в любом регистре, дефиса и пробела
-					input.value = input.value.replace((/[^а-яА-Я-\s]/), '');
-					// Первая буква заглавная, остальные прописные
-					input.value = input.value.replace(/(|\s+)\S/g, val => val.toLowerCase());
-					input.value = input.value.replace(/(^|\s)\S/g, val => val.toUpperCase());
-				});
+
 			} else if (input.getAttribute('name') === 'user_message') {
 				if ((/[а-яА-Я\s-]/).test(input.value)) {
-					input.value = input.value.replace((/[^а-яА-Я-\s]/gi), '');
-				}
-				input.addEventListener('input', () => {
 					// разрешен только ввод кириллицы в любом регистре, дефиса и пробела
-					input.value = input.value.replace((/[^а-яА-Я-\s]/), '');
-				});
+					input.value = input.value.replace((/[^а-яА-Я-\s]/gi), '');
+					// убираем удваивающиеся пробелы и дефисы вне фокуса после вставки
+					input.value = input.value.replace((/[\s]+/g), ' ');
+					input.value = input.value.replace((/[-]+/gi), '-');
+				}
+
 			} else if (input.getAttribute('name') === 'user_email') {
 				if ((/[^a-zA-z-@_.!~*']/).test(input.value)) {
-					input.value = input.value.replace((/[^a-zA-z@-_.!~*']/gi), '');
-				}
-				input.addEventListener('input', () => {
 					// разрешен только ввод ввод латиницы в любом регистре и спецсимволы
-					input.value = input.value.replace((/[^a-zA-z-@_.!~*']/), '');
-				});
+					input.value = input.value.replace((/[^a-zA-z@-_.!~*']/gi), '');
+					// убираем удваивающиеся пробелы и дефисы вне фокуса после вставки
+					input.value = input.value.replace((/[\s]+/g), ' ');
+					input.value = input.value.replace((/[-]+/gi), '-');
+				}
+
 			} else if (input.getAttribute('name') === 'user_phone') {
 				if ((/[^\d()-]/).test(input.value)) {
-					input.value = input.value.replace(/[^\d()-]+/gi, '');
-				}
-				input.addEventListener('input', () => {
 					// разрешен только ввод цифр, круглых скобок и дефис
-					input.value = input.value.replace((/[^\d()-]+/), '');
-				});
+					input.value = input.value.replace((/[^\d()-]+/g), '');
+					// убираем удваивающиеся пробелы и дефисы вне фокуса после вставки
+					input.value = input.value.replace((/[\s]+/g), ' ');
+					input.value = input.value.replace((/[-]+/gi), '-');
+				}
 			}
 		});
 	});
@@ -120,7 +144,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		const idInterval = setInterval(updateClock, 1000);
 	}
 
-	countTimer('30 april 2021');
+	countTimer('30 april 2023');
 
 	// плавный скролл меню
 	const scroll = () => {
