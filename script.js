37fe5c66225b942f16665a0beb0a527672bf2f44
Ksenia Pogorelova <ksenia.harina5@gmail.com>
2021-05-05 16:21:10 +0300

    Скрипт переписан для получения данных, используя промисы

--- script.js
+++ script.js
@@ -4,25 +4,44 @@ document.addEventListener('DOMContentLoaded', () => {
     const select = document.getElementById('cars'),
         output = document.getElementById('output');
 
-    select.addEventListener('change', () => {
+    const getCars = () => new Promise((response, reject) => {
         const request = new XMLHttpRequest();
         request.open('GET', './cars.json');
         request.setRequestHeader('Content-type', 'application/json');
         request.send();
+
         request.addEventListener('readystatechange', () => {
-            if (request.readyState === 4 && request.status === 200) {
+            if (request.readyState !== 4) {
+                return;
+            }
+            if (request.status === 200) {
                 const data = JSON.parse(request.responseText);
-                data.cars.forEach(item => {
-                    if (item.brand === select.value) {
-                        const { brand, model, price } = item;
-                        output.innerHTML = `Тачка ${brand} ${model} <br>
-                        Цена: ${price}$`;
-                    }
-                });
+                response(data);
             } else {
-                output.innerHTML = 'Произошла ошибка';
+                reject(request.statusText);
             }
         });
+    });
+
+    const outputData = data => {
+        data.cars.forEach(item => {
+            if (item.brand === select.value) {
+                const { brand, model, price } = item;
+                output.innerHTML = `Тачка ${brand} ${model} <br>
+                Цена: ${price}$`;
+            }
+        });
+    };
+
+    const errorData = error => {
+        output.innerHTML = 'Произошла ошибка';
+        console.error(error);
+    };
+
+    select.addEventListener('change', () => {
+        getCars()
+            .then(outputData)
+            .catch(error => errorData(error));
     });
 
 });
